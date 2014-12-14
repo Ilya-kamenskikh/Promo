d100e4b7b73fe0a88a3f056147593d464b1b305c
Nazarenkr434675 <Nazarenkr434675@5de74c49-4db9-43bc-9ab8-54ba8e52701d>
2014-12-14 19:28:47 +0000

    Roman

--- PromoVideos/src/com/promo/Model.java
+++ PromoVideos/src/com/promo/Model.java
@@ -1,5 +1,27 @@
 package com.promo;
 
 public class Model {
-
+	public ArrayList <String> companies = new ArrayList <String>;//Контейнер именкомпаний
+	public void addCompany(name){
+		String Name;
+		this.name = Name;
+		companies.add(Name);
+		String dir = "C:/companies/"+Name
+		File f = new File(dir);
+		f.mkdir();
+	}
+	public void addMovie(name, company, address){
+		String company, name, address;
+		this.company = company;
+		this.name = name;
+		this.adress = address
+		File f = new File(("C:/companies/" + company + name).txt)
+		f.createNewFile();
+		BufferedWriter in = new BufferedWriter(f);
+		in.write(address, 1,1000);
+	}
+	public void editCompany(name){
+		String name;
+		this.name = name
+	}
 }
